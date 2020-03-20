6826378a8f39631b58a6dc8699f9deb99d21347c
Rai <raimuhammadansar@gmail.com>
2020-03-21 02:02:20 +0500

    2nd Commit

--- Dart_Book/G.dart
+++ Dart_Book/G.dart
@@ -1,22 +1,28 @@
 main() {
-  List fruitCollection = [
-    "mangoes",
-    "Oranges",
-    "Jack Fruit",
-    "Water Melons",
-    "Gand Melone"
-  ];
+  // List fruitCollection = [
+  //   "mangoes",
+  //   "Oranges",
+  //   "Jack Fruit",
+  //   "Water Melons",
+  //   "Gand Melone"
+  // ];
 
-  dynamic myInteger = [1, 3, " 4, 6, 7, 8, 9", "Qeema"];
-  print("Selected List part is: ${myInteger[2]}");
-  for (var number in myInteger) {
-    print(number);
-  }
-  // print(fruitCollection);
-
-  // for (var fruits in fruitCollection) {
-  //   print(fruits);
+  // dynamic myInteger = [1, 3, " 4, 6, 7, 8, 9", "Qeema"];
+  // print("Selected List part is: ${myInteger[2]}");
+  // for (var number in myInteger) {
+  //   print(number);
   // }
+  // // print(fruitCollection);
 
-  
+  // // for (var fruits in fruitCollection) {
+  // //   print(fruits);
+  // // }
+
+  var fruitCollection = {' Mango', 'Apple', 'WaterMelon', 'Peanut'};
+
+  for (var fruit in fruitCollection) {
+    print(fruit);
+  }
+
+  print(fruitCollection.lookup("AppleS"));
 }
